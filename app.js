fabe2846db777a46efb17a34b63fac50465ff842
YELLAPUHEMAVENKATA <anilyallapu12345@gmail.com>
2025-08-03 11:45:32 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,26 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const authRouter = require("./routes/authRoutes.js");
+
+const port = process.env.PORT;
+const db_url = process.env.MONGO_URL;
+const app = express();
+
+app.use(cors({
+  origin: 'http://localhost:5173',
+  credentials: true // If you're sending cookies or auth headers
+}));
+app.use(express.json());
+
+app.use("/auth", authRouter);
+
+mongoose
+  .connect(db_url)
+  .then(() => console.log("db connected"))
+  .catch((err) => console.log(err.message));
+
+app.listen(port, () => {
+  console.log(`server running on port ${port}`);
+});
